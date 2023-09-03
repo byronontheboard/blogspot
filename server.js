4b88a2c77b245a91696483b039a0bec3cea44780
byronallan <127366720+byronallan@users.noreply.github.com>
2023-09-02 20:39:09 -0700

    made all necessary installations for the application

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 // Configure Express to use Express Handlebars
 app.engine('handlebars', hbs.engine);
