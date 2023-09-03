8d5d3eca2f87ab39d07e807f04a82e0c6b49b304
byronallan <127366720+byronallan@users.noreply.github.com>
2023-09-02 23:20:04 -0700

    can't get app to run when I enter npm start

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
+
+// Configure and link a session object with the sequelize store
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+  
+// Add express-session and store as Express.js middleware
+app.use(session(sess));
 
 // Configure Express to use Express Handlebars
 app.engine('handlebars', hbs.engine);
