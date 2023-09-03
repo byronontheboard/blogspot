c21109fe9c00df58562ee8d5ad4a21c69705d6d0
byronallan <127366720+byronallan@users.noreply.github.com>
2023-09-02 20:19:33 -0700

    mixup with routes fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,39 +1,10 @@
 const router = require('express').Router();
+
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const userRoutes = require('./userRoutes');
-const { User } = require('../models');
 
+
+router.use('/api', apiRoutes);
 router.use('/home', homeRoutes);
 router.use('/users', userRoutes);
-
-// Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const userData = await User.findAll({
-            attributes: { exclude: ['password'] },
-            order: [['name', 'ASC']],
-        });
-    
-        const users = userData.map((project) => project.get({ plain: true }));
-    
-        res.render('homepage', {
-            users,
-            // Pass the logged in flag to the template
-            logged_in: req.session.logged_in,
-        });
-        } catch (err) {
-        res.status(500).json(err);
-    }
-});
-  
-router.get('/login', (req, res) => {
-    // If a session exists, redirect the request to the homepage
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-    }
-
-res.render('login');
-});
-  
-module.exports = router;
